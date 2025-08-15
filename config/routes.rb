cc1eb28b02f973fd65fde0994510d5e6f9ca419b
Michael Henderson <m.nishimoto97@gmail.com>
2025-08-15 18:32:26 +0900

    Testing

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  #Defines list paths
+  resources :lists, only: %i[index show new create] do
+    resources :bookmarks, only: %i[new create]
+  end
+
+  resources :bookmarks, only: [:destroy]
 end
